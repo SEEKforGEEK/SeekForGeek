2041ba89111f7fad3752ba73f3a2552e1caae04d
ValentinAlexandrovGeorgiev <valentin.georgiev95@abv.bg>
2015-12-12 00:39:52 +0200

    Added register, log in and forgot password forms

--- javascript/home.js
+++ javascript/home.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 	$("#navbarTemplate").css("display","none");
+
+	$('#loginInRegister').click(loginInRegister);
+	$('#registerInLogin').click(registerInLogin);
+	$('#btnForgotPass').click(registerInLogin);
+
+
 });
+
+function loginInRegister(){
+	$("#closeRegister").trigger("click");
+}
+function registerInLogin(){
+	$("#closeLogIn").trigger("click");
+}
