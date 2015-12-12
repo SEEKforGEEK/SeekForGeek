23ff90c6197fd955bf53cdd9c86bc8973eea158a
ValentinAlexandrovGeorgiev <valentin.georgiev95@abv.bg>
2015-12-12 11:43:32 +0200

    add validation to register form

--- javascript/setup/home.js
+++ javascript/setup/home.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 	$('#registerInLogin').click(registerInLogin);
 	$('#btnForgotPass').click(registerInLogin);
 
+	$('#registerButton').click(registerForm);
+
 
 });
 
@@ -13,4 +15,47 @@ function loginInRegister(){
 }
 function registerInLogin(){
 	$("#closeLogIn").trigger("click");
+}
+function registerForm(){
+
+	var type;
+	if($('#register-hire').is(':checked')){
+		type = 'customer';
+	}
+	if($('#register-work').is(':checked')) {
+		type = 'geek';
+	}
+
+	var registerData = {
+		email: $('#register-email').val(),
+		username:  $('#register-username').val(),
+		password: $('#register-pass').val(),
+		type: type
+	};
+
+	if(validateRegisterForm(registerData)){
+		//todo save in parse.com
+	}else{
+		//todo
+	}
+}
+
+function validateRegisterForm(registerData){
+	var patternForEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	var confirmPassword = $("#register-conf-pass").val();
+
+	if(!registerData.email.match(patternForEmail) || registerData.username.length < 6){
+		//todo red div
+		return false;
+	}
+	if(registerData.password.length < 6){
+		return false;
+	}
+	if(registerData.type != 'customer' && registerData.type != 'geek'){
+		return false;
+	}
+	if(registerData.password !== confirmPassword){
+		return false;
+	}
+	return true;
 }
