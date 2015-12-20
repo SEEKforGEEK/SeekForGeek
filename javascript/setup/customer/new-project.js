1b53dfbfe76192e81d93d1c4cb221f43a47cf1b3
ValentinAlexandrovGeorgiev <valentin.al.georgiev@gmail.com>
2015-12-20 15:43:08 +0200

    new project with customer

---
+++ javascript/setup/customer/new-project.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+ 	$(".navbarTemplate")
+        .removeClass("navbarTemplateNone")
+        .addClass("navbarTemplateOn");
+
+
+	$('#details-next').click(function(){
+		$('#description-link').attr('href', '#description')
+		.attr('data-toggle', 'tab')
+		.attr('aria-expanded', true);
+    	
+    	$('#description-link').trigger('click');
+    	$('#details-link').removeAttr('href')
+    		.removeAttr('aria-expanded')
+    		.removeAttr('data-toggle');
+    });
+	$('#description-next').click(function(){  		
+  
+
+  		$('#payment-link').attr('href', '#payment')
+  			.attr('data-toggle', 'tab')
+  			.attr('aria-expanded', true);
+
+		$('#payment-link').trigger('click');
+
+
+		$('#description-link').removeAttr('href')
+		.removeAttr('aria-expanded')
+		.removeAttr('data-toggle');
+
+
+
+    });
+})
