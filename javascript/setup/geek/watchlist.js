8a04a0242b9759b2e63103a3d1fa4d229012261a
ValentinAlexandrovGeorgiev <valentin.al.georgiev@gmail.com>
2015-12-23 22:02:37 +0200

    remove from watchlist but have 2 bugs

--- javascript/setup/geek/watchlist.js
+++ javascript/setup/geek/watchlist.js
@@ -21,6 +21,7 @@ $(document).ready(function(){
 	}
 	else{
 		var query = new Parse.Query("Projects");
+		console.log(watchlistArray);
 		for (var i = 0; i < watchlistArray.length; i++) {
 
 			query.equalTo('objectId', watchlistArray[i]);
@@ -34,7 +35,7 @@ $(document).ready(function(){
 			               '<td class="project-category">' + res.get('type') + '</td>' +
 			               '<td class="project-date">' + res.get('endDate') + '</td>' +
 			               '<td class="project-price">' + res.get('price') + '</td>' +
-			               '<td><button id="remove-' + res.id + '" class="button">Remove</button></td>' +
+			               '<td><button data-id="'+ res.id + '" class="button remove-buttons">Remove</button></td>' +
 			            '</tr>'
 					);
 				},
@@ -44,7 +45,28 @@ $(document).ready(function(){
 			});
 		}	
 	}
+		console.log(watchlistArray);
 
+
+
+	$('body').on('click', '.remove-buttons', function(event){
+
+
+		var id = $('.remove-buttons').data('id');
+		var newArray = [],
+			j = 0;
+		for (var i = 0; i < watchlistArray.length; i++) {
+			if (watchlistArray[i] != id){
+				newArray[j++] = watchlistArray[i];
+			};
+		};
+
+
+
+		currentUser.set('watchlist', newArray);
+		currentUser.save();
+		$(this).parent().parent().remove();
+	})
 
 	
 });
