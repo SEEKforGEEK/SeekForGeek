a3ab865ab8ef5561caac8def05f46be742f111f7
ValentinAlexandrovGeorgiev <valentin.al.georgiev@gmail.com>
2015-12-24 18:04:10 +0200

    add more functionality, but js cry for refactoring

--- javascript/setup/search.js
+++ javascript/setup/search.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
         .removeClass("templateNone")
         .addClass("templateOn");
 
-
+	var currentUser = Parse.User.current();    
+	
     $('#all-projects').on('click', function(){
     	$(location).attr('href', '/#/search?index=all');
     });
@@ -32,12 +33,20 @@ $(document).ready(function(){
 		
 	var index = $.urlParam('index');
 
-	switch(index){
-		case 'all': allQuery(); break;
-		case 'dev': devProjects(); break;
-		case 'design': designProjects(); break;
+	
+	if (currentUser.get('type') == 'customer') {
+		switch(index){
+			case 'all': customerAllQuery(); break;
+			case 'dev': customerDevProjects(); break;
+			case 'design': customerDesignProjects(); break;
+		}
+	}else{
+		switch(index){
+			case 'all': allQuery(); break;
+			case 'dev': devProjects(); break;
+			case 'design': designProjects(); break;
+		}
 	}
-
 
     // $('#all-projects').on('click', function(){
     // $(location).attr('href', '/#/search?index=all');
@@ -50,33 +59,14 @@ $(document).ready(function(){
 
 
 function allQuery(){
+	var currentUser = Parse.User.current();  	
+	var watchlistArray = currentUser.get('watchlist');
 	var query = new Parse.Query('Projects');
 	query.exists('title');
 	query.find({
 		success: function(res){
 			for (var i = 0; i < res.length; i++) {
-				$('#append-projects').append(
-					'<tr>' +
-		               '<td class="search-project-title"><a href="/#/project-details?id='+ 
-		               res[i].id + '">' + res[i].get('title') + '</a></td>' +
-		               '<td class="search-project-category">' + res[i].get('type') + '</td>' +
-		               '<td class="search-project-date">' + res[i].get('endDate') + '</td>' +
-		               '<td class="search-project-price">' + res[i].get('price') + '</td>' +
-		               '<td><button data-id="'+ res[i].id + '" class="button remove-buttons">Add to watchlist</button></td>' +
-		            '</tr>'
-				);
-			};
-		}
-	});
-}
-
-function devProjects(){
-	var query = new Parse.Query('Projects');
-	query.exists('type');
-	query.find({
-		success: function(res){
-			for (var i = 0; i < res.length; i++) {
-				if (checkTypeDev(res[i].get('type'))) {
+				if (checkWatchlist(watchlistArray, res[i].id)) {
 					$('#append-projects').append(
 						'<tr>' +
 			               '<td class="search-project-title"><a href="/#/project-details?id='+ 
@@ -84,9 +74,56 @@ function devProjects(){
 			               '<td class="search-project-category">' + res[i].get('type') + '</td>' +
 			               '<td class="search-project-date">' + res[i].get('endDate') + '</td>' +
 			               '<td class="search-project-price">' + res[i].get('price') + '</td>' +
-			               '<td><button data-id="'+ res[i].id + '" class="button remove-buttons">Add to watchlist</button></td>' +
+			               '<td class="watchlist-func"><button data-id="'+ res[i].id + '" class="button add-to-watchlist">Add to watchlist</button></td>' +
 			            '</tr>'
-					);	
+					);
+				}else{
+					$('#append-projects').append(
+						'<tr>' +
+			               '<td class="search-project-title"><a href="/#/project-details?id='+ 
+			               res[i].id + '">' + res[i].get('title') + '</a></td>' +
+			               '<td class="search-project-category">' + res[i].get('type') + '</td>' +
+			               '<td class="search-project-date">' + res[i].get('endDate') + '</td>' +
+			               '<td class="search-project-price">' + res[i].get('price') + '</td>' +
+			            '</tr>'
+					);
+				}
+			};
+		}
+	});
+}
+
+function devProjects(){
+	var currentUser = Parse.User.current();  	
+	var watchlistArray = currentUser.get('watchlist');
+	var query = new Parse.Query('Projects');
+	query.exists('type');
+	query.find({
+		success: function(res){
+			for (var i = 0; i < res.length; i++) {
+				if (checkTypeDev(res[i].get('type'))) {
+					if (checkWatchlist(watchlistArray, res[i].id)) {
+						$('#append-projects').append(
+							'<tr>' +
+				               '<td class="search-project-title"><a href="/#/project-details?id='+ 
+				               res[i].id + '">' + res[i].get('title') + '</a></td>' +
+				               '<td class="search-project-category">' + res[i].get('type') + '</td>' +
+				               '<td class="search-project-date">' + res[i].get('endDate') + '</td>' +
+				               '<td class="search-project-price">' + res[i].get('price') + '</td>' +
+				               '<td class="watchlist-func"><button data-id="'+ res[i].id + '" class="button add-to-watchlist">Add to watchlist</button></td>' +
+				            '</tr>'
+						);
+					}else{
+						$('#append-projects').append(
+							'<tr>' +
+				               '<td class="search-project-title"><a href="/#/project-details?id='+ 
+				               res[i].id + '">' + res[i].get('title') + '</a></td>' +
+				               '<td class="search-project-category">' + res[i].get('type') + '</td>' +
+				               '<td class="search-project-date">' + res[i].get('endDate') + '</td>' +
+				               '<td class="search-project-price">' + res[i].get('price') + '</td>' +
+				            '</tr>'
+						);
+					}
 				};
 				
 			};
@@ -95,22 +132,36 @@ function devProjects(){
 }
 
 function designProjects(){
+	var currentUser = Parse.User.current();  	
+	var watchlistArray = currentUser.get('watchlist');
 	var query = new Parse.Query('Projects');
 	query.exists('type');
 	query.find({
 		success: function(res){
 			for (var i = 0; i < res.length; i++) {
 				if (!checkTypeDev(res[i].get('type'))) {
-					$('#append-projects').append(
-						'<tr>' +
-			               '<td class="search-project-title"><a href="/#/project-details?id='+ 
-			               res[i].id + '">' + res[i].get('title') + '</a></td>' +
-			               '<td class="search-project-category">' + res[i].get('type') + '</td>' +
-			               '<td class="search-project-date">' + res[i].get('endDate') + '</td>' +
-			               '<td class="search-project-price">' + res[i].get('price') + '</td>' +
-			               '<td><button data-id="'+ res[i].id + '" class="button remove-buttons">Add to watchlist</button></td>' +
-			            '</tr>'
-					);	
+					if (checkWatchlist(watchlistArray, res[i].id)) {
+						$('#append-projects').append(
+							'<tr>' +
+				               '<td class="search-project-title"><a href="/#/project-details?id='+ 
+				               res[i].id + '">' + res[i].get('title') + '</a></td>' +
+				               '<td class="search-project-category">' + res[i].get('type') + '</td>' +
+				               '<td class="search-project-date">' + res[i].get('endDate') + '</td>' +
+				               '<td class="search-project-price">' + res[i].get('price') + '</td>' +
+				               '<td class="watchlist-func"><button data-id="'+ res[i].id + '" class="button add-to-watchlist">Add to watchlist</button></td>' +
+				            '</tr>'
+						);
+					}else{
+						$('#append-projects').append(
+							'<tr>' +
+				               '<td class="search-project-title"><a href="/#/project-details?id='+ 
+				               res[i].id + '">' + res[i].get('title') + '</a></td>' +
+				               '<td class="search-project-category">' + res[i].get('type') + '</td>' +
+				               '<td class="search-project-date">' + res[i].get('endDate') + '</td>' +
+				               '<td class="search-project-price">' + res[i].get('price') + '</td>' +
+				            '</tr>'
+						);
+					}
 				};
 				
 			};
@@ -128,4 +179,79 @@ function checkTypeDev(str){
 		}
 	};
 	return true;
+}
+function checkWatchlist(watchlist, str){
+	for (var i = 0; i < watchlist.length; i++) {
+		if (watchlist[i] == str) {
+			return true;
+		};
+	};
+	return false;
+}
+
+
+function customerAllQuery(){
+	var query = new Parse.Query('Projects');
+	query.exists('title');
+	query.find({
+		success: function(res){
+			for (var i = 0; i < res.length; i++) {
+				$('#append-projects').append(
+					'<tr>' +
+		               '<td class="search-project-title"><a href="/#/project-details?id='+ 
+		               res[i].id + '">' + res[i].get('title') + '</a></td>' +
+		               '<td class="search-project-category">' + res[i].get('type') + '</td>' +
+		               '<td class="search-project-date">' + res[i].get('endDate') + '</td>' +
+		               '<td class="search-project-price">' + res[i].get('price') + '</td>' +
+		            '</tr>'
+				);
+			};
+		}
+	});
+}
+
+function customerDevProjects(){
+	var query = new Parse.Query('Projects');
+	query.exists('type');
+	query.find({
+		success: function(res){
+			for (var i = 0; i < res.length; i++) {
+				if (checkTypeDev(res[i].get('type'))) {
+					$('#append-projects').append(
+						'<tr>' +
+			               '<td class="search-project-title"><a href="/#/project-details?id='+ 
+			               res[i].id + '">' + res[i].get('title') + '</a></td>' +
+			               '<td class="search-project-category">' + res[i].get('type') + '</td>' +
+			               '<td class="search-project-date">' + res[i].get('endDate') + '</td>' +
+			               '<td class="search-project-price">' + res[i].get('price') + '</td>' +
+			            '</tr>'
+					);	
+				};
+				
+			};
+		}
+	})
+}
+
+function customerDesignProjects(){
+	var query = new Parse.Query('Projects');
+	query.exists('type');
+	query.find({
+		success: function(res){
+			for (var i = 0; i < res.length; i++) {
+				if (!checkTypeDev(res[i].get('type'))) {
+					$('#append-projects').append(
+						'<tr>' +
+			               '<td class="search-project-title"><a href="/#/project-details?id='+ 
+			               res[i].id + '">' + res[i].get('title') + '</a></td>' +
+			               '<td class="search-project-category">' + res[i].get('type') + '</td>' +
+			               '<td class="search-project-date">' + res[i].get('endDate') + '</td>' +
+			               '<td class="search-project-price">' + res[i].get('price') + '</td>' +
+			            '</tr>'
+					);	
+				};
+				
+			};
+		}
+	})
 }
