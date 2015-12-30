6d34569ed5d78dbe13ab774ed147f05720ff0585
elenastoyanovaa <elenastoyanovaaa@gmail.com>
2015-12-30 23:40:17 +0200

    added contact us page

---
+++ javascript/contact-us.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".navbarTemplate")
+        .removeClass("templateNone")
+        .addClass("templateOn");
+    $("#footerTemplate")
+        .removeClass("templateNone")
+        .addClass("templateOn");
+});
