f80cff4330897e56d76e895a363e408d8e7d5120
ValentinAlexandrovGeorgiev <valentin.al.georgiev@gmail.com>
2015-12-31 15:23:41 +0200

    save messages

--- javascript/contact-us.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$(".navbarTemplate")
-        .removeClass("templateNone")
-        .addClass("templateOn");
-    $("#footerTemplate")
-        .removeClass("templateNone")
-        .addClass("templateOn");
-});
---
+++ javascript/setup/contact-us.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	$(".navbarTemplate")
+        .removeClass("templateNone")
+        .addClass("templateOn");
+    $("#footerTemplate")
+        .removeClass("templateNone")
+        .addClass("templateOn");
+
+	var mail = {};
+    var currentUser = Parse.User.current();
+    var Messages = new Parse.Object("Messages");
+    $('#send-message').on('click', function(event){
+    	event.preventDefault();
+	   
+	    mail.currentEmail = $('#email').val() || currentUser.get('email');
+	    mail.firstName = $('#fname').val();
+	    mail.lastName = $('#lname').val();
+	    mail.phone = $('#phone').val();
+	    mail.message = $('#message').val();
+	    
+	    Messages.set('firstName', mail.firstName);
+	    Messages.set('lastName', mail.lastName);
+	    Messages.set('email', mail.currentEmail);
+	    Messages.set('phone', mail.phone);
+	    Messages.set('message', mail.message);
+
+	    Messages.save()
+	    	.then(function(){
+	    		toastr.success('Successfully send message! Thank you!');
+	    		$('#email').val('');
+	    		$('#fname').val('');
+	    		$('#lname').val('');
+	    		$('#phone').val('');
+	    		$('#message').val('');
+	    	});
+	});
+});
