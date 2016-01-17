baf38d094697aaabc79b8d0217064f28ca14ab3d
ValentinAlexandrovGeorgiev <valentin.georgiev95@abv.bg>
2016-01-17 12:53:29 +0200

    refactoring all-projects.js (geek)

--- javascript/setup/geek/all-projects.js
+++ javascript/setup/geek/all-projects.js
@@ -1,151 +1,180 @@
-$(document).ready(function(){
-	$(".navbarTemplate")
-        .removeClass("templateNone")
-        .addClass("templateOn");
-    $("#footerTemplate")
-        .removeClass("templateNone")
-        .addClass("templateOn");
+'use strict';
 
-	$('.browse-job').on('click',function(){
-		$(location).attr('href', '/#/search?index=all');
-	});
-
-	var currentUser = Parse.User.current();
-	var watchlistArray = currentUser.get('watchlist') || [];
-	var currentName = currentUser.get('username');
-    $('#watchlist-badge').html(watchlistArray.length);
-   
-    
-	
-	var query = new Parse.Query("Submissions");
-
-	query.equalTo('submissionOwner', currentName);
-	query.find({
-		success: function(res){
-            $('#projects-badge').html(res.length);
-
-			if (res.length == 0) {
-				$('#append-body-projects').append(
-					'<h1>No submissions</h1>'
-				);
-			}
-			else{
-				var pages;
-				if (res.length % 5 == 0) {
-					pages = res.length / 5;
-				}else{
-					pages = ((res.length / 5) | 0) + 1;
-				}
-
-				$('#pagination-demo').twbsPagination({
-					totalPages: pages,
-					visiblePages: 5,
-					onPageClick: function (event, page) {
-						$('#append-body-projects').empty();
-						var index = page * 5;
-						var grade;
-						for (var i = index - 5; i < index; i++) {
-							if (res.length <= i ) {
-								break;
-							};
-							if (res[i].get('grade') == 'in progress') {
-								grade = 'progress';
-							}else if(res[i].get('grade') == 'winner'){
-								grade = 'winner';
-							}else{
-								grade = 'notWinner';
-							}
-							$('#append-body-projects').append(
-								'<tr>' +
-					               '<td>' + res[i].get('title') + '</td>' +
-					               '<td>' + res[i].get('projectOwner') + '</td>' +
-					               '<td>' + res[i].get('endDate') + '</td>' +
-					               '<td>' + res[i].get('price') + '</td>' +
-					               '<td><a href="' + res[i].get('files').url() + '">Download</a></td>' +
-					               '<td><button data-id="' + res[i].id + '" data-toggle="modal" data-target="#' + grade + '" class="button check-project-winner">+</button></td>' +	
-					            '</tr>'
-							);
-						}
-					}
-				});
-			}
-			
+var AllProjectModule = function(settings){
+	var options = {
+		parse: {
 		},
-		error: function(err){
-			toastr.error('Sorry something happen, please to log out and log in again!');
+		currentUser: {
+		},
+		selectors: {
+			searchLink: '/#/search?index=all',
+			watchlistBadge: '#watchlist-badge',
+			projectsBadge: '#projects-badge',
+			bodyProjects: '#append-body-projects',
+			pagination: '#pagination-demo',
+			checkWinner: '.check-project-winner',
+			cardHolder: '#card-holder',
+			IBAN: '#IBAN',
+			cardHolderForm: '#payment-cardholer-form',
+			IBANForm: '#payment-IBAN-form',
+			btnBrowseJob: '.browse-job',
+			btnPaymentInfo: '#payment-info-btn',
+			btnClose: '.close'
+		},
+		variables: {
+			projects: []
 		}
-	});
-	
+	},
 
-	$('#append-body-projects').on('click', '.check-project-winner', function(event){
-		event.preventDefault();
-		var id = $(this).attr('data-id');
+	initialize = function(){
+		jQuery(".navbarTemplate")
+			.removeClass("templateNone")
+			.addClass("templateOn");
+		jQuery("#footerTemplate")
+			.removeClass("templateNone")
+			.addClass("templateOn");
 
-		winner(id);
-	});
-	
+		jQuery(options.selectors.btnBrowseJob).on('click', function(){
+			jQuery(location).attr('href', options.selectors.searchLink);
+		});
 
+		options.parse.currentUser = Parse.User.current();
+		options.currentUser.watchlist = options.parse.currentUser.get('watchlist') || [];
+		options.currentUser.username = options.parse.currentUser.get('username');
 
+		jQuery(options.selectors.watchlistBadge).html(options.currentUser.watchlist.length);
 
-});
+		options.parse.submissionsQuery = new Parse.Query("Submissions");
 
+		options.parse.submissionsQuery.equalTo('submissionOwner', options.currentUser.username);
+		options.parse.submissionsQuery.find({
+			success: function(res){
+				jQuery(options.selectors.projectsBadge).html(res.length);
 
-function winner(id){
-	var query = new Parse.Query("Submissions");
-	query.equalTo('objectId', id);
-	query.first({
-		success: function(res){
-			if (res.get('grade') == 'winner') {
-				$('#payment-info-btn').on('click',function(event){
-
-					event.preventDefault();
-					var paymentData = {};
-					paymentData.cardHolder = $('#card-holder').val();
-					paymentData.IBAN = $('#IBAN').val();
-					if(validateWinnerForm(paymentData)){
-						res.set('payment', paymentData);
-						res.save()
-							.then(function(){
-								console.log('sadad');
-
-								$('.close').trigger('click');
-							})	
+				if (res.length == 0) {
+					jQuery(options.selectors.bodyProjects).append(
+						'<h1>No submissions</h1>'
+					);
+				}
+				else{
+					var pages;
+					if (res.length % 5 == 0) {
+						pages = res.length / 5;
+					}else{
+						pages = ((res.length / 5) | 0) + 1;
 					}
-					
-				})
+
+					jQuery(options.selectors.pagination).twbsPagination({
+						totalPages: pages,
+						visiblePages: 5,
+						onPageClick: function (event, page) {
+							jQuery(options.selectors.bodyProjects).empty();
+							var index = page * 5;
+							var grade;
+							for (var i = index - 5; i < index; i++) {
+								if (res.length <= i ) {
+									break;
+								}
+								if (res[i].get('grade') == 'in progress') {
+									grade = 'progress';
+								}else if(res[i].get('grade') == 'winner'){
+									grade = 'winner';
+								}else{
+									grade = 'notWinner';
+								}
+								$(options.selectors.bodyProjects).append(tableRow(res[i], grade));
+							}
+						}
+					});
+				}
+			},
+			error: function(){
+				toastr.error('Sorry something happen, please to log out and log in again!');
 			}
+		});
+
+		jQuery(options.selectors.bodyProjects).on('click', options.selectors.checkWinner, function(event){
+			event.preventDefault();
+			var id = jQuery(this).attr('data-id');
+
+			winner(id);
+		});
+	},
+
+	tableRow = function(project, grade){
+		return '<tr>' +
+			'<td>' + project.get('title') + '</td>' +
+			'<td>' + project.get('projectOwner') + '</td>' +
+			'<td>' + project.get('endDate') + '</td>' +
+			'<td>' + project.get('price') + '</td>' +
+			'<td><a href="' + project.get('files').url() + '">Download</a></td>' +
+			'<td><button data-id="' + project.id + '" data-toggle="modal" data-target="#' + grade + '" class="button check-project-winner">+</button></td>' +
+		'</tr>'
+	},
+
+	winner = function(id){
+		options.parse.submissionsQuery.equalTo('objectId', id);
+		options.parse.submissionsQuery.first({
+			success: function(res){
+				if (res.get('grade') == 'winner') {
+					jQuery(options.selectors.btnPaymentInfo).on('click',function(event){
+						event.preventDefault();
+
+						var paymentData = {};
+						paymentData.cardHolder = jQuery(options.selectors.cardHolder).val();
+						paymentData.IBAN = jQuery(options.selectors.IBAN).val();
+
+						if(validateWinnerForm(paymentData)){
+							res.set('payment', paymentData);
+							res.save()
+								.then(function(){
+									jQuery().trigger('click');
+								});
+						}
+
+					})
+				}
+			}
+		})
+	},
+
+	validateWinnerForm = function(data){
+		var cardHolderForm = jQuery(options.selectors.cardHolderForm);
+		var IBANForm = jQuery(options.selectors.IBANForm);
+
+		if(data.cardHolder.split(' ').length == 2) {
+			if (cardHolderForm.hasClass('has-error')) {
+				cardHolderForm.removeClass('has-error');
+			}
+			cardHolderForm.addClass('has-success');
+		}else{
+			if (cardHolderForm.hasClass('has-success')) {
+				cardHolderForm.removeClass('has-success');
+			}
+			cardHolderForm.addClass('has-error');
+			return false;
 		}
-	})
-}
 
-function validateWinnerForm(data){
-	var cardHolder = $('#payment-cardholer-form');
-	var IBAN = $('#payment-IBAN-form');
+		if (data.IBAN.length == 16) {
+			if (IBANForm.hasClass('has-error')) {
+				IBANForm.removeClass('has-error');
+			}
+			IBANForm.addClass('has-success');
+		}else{
+			if (IBANForm.hasClass('has-success')) {
+				IBANForm.removeClass('has-success');
+			}
+			IBANForm.addClass('has-error');
+			return false;
+		}
+		return true;
+	};
+	return{
+		initialize: initialize
+	};
+};
 
-	if(data.cardHolder.split(' ').length == 2) {
-		if (cardHolder.hasClass('has-error')) {
-            cardHolder.removeClass('has-error');
-        }
-        cardHolder.addClass('has-success');
-	}else{
-		if (cardHolder.hasClass('has-success')) {
-            cardHolder.removeClass('has-success');
-        }
-        cardHolder.addClass('has-error');
-        return false;
-	}
 
-	if (data.IBAN.length == 16) {
-		if (IBAN.hasClass('has-error')) {
-            IBAN.removeClass('has-error');
-        }
-        IBAN.addClass('has-success');
-	}else{
-		if (IBAN.hasClass('has-success')) {
-            IBAN.removeClass('has-success');
-        }
-        IBAN.addClass('has-error');
-        return false;
-	}
-	return true;
-}
+jQuery(document).ready(function() {
+	AllProjectModule().initialize();
+});
